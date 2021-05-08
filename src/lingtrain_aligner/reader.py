248fd64549652f691a4995c57b65062ff9faa33a
Sergei Averkiev <averoO@gmail.com>
2021-05-08 17:29:22 +0300

    Add headers. Add styles.

--- src/lingtrain_aligner/reader.py
+++ src/lingtrain_aligner/reader.py
@@ -38,7 +38,7 @@ def get_paragraphs(db_path):
     prev_meta = paragraphs_from_dict[json.loads(index[0][0][1])[0]]
 
     prev_paragraph_from = prev_meta[0]
-    prev_h1, prev_h2, prev_di = prev_meta[1], prev_meta[2], prev_meta[6]
+    prev_h1, prev_h2, prev_h3, prev_h4, prev_h5, prev_di = prev_meta[1], prev_meta[2], prev_meta[3], prev_meta[4], prev_meta[5], prev_meta[6]
 
     curr_from, curr_to = [data[0]["text_from"]], [data[0]["text_to"]]
 
@@ -46,8 +46,12 @@ def get_paragraphs(db_path):
         fid = max(json.loads(item[0][1]))
 
         curr_paragraph_from = paragraphs_from_dict[fid][0]
+
         curr_h1 = paragraphs_from_dict[fid][1]
         curr_h2 = paragraphs_from_dict[fid][2]
+        curr_h3 = paragraphs_from_dict[fid][3]
+        curr_h4 = paragraphs_from_dict[fid][4]
+        curr_h5 = paragraphs_from_dict[fid][5]
         curr_di = paragraphs_from_dict[fid][6]
 
         if curr_paragraph_from == prev_paragraph_from:
@@ -70,6 +74,21 @@ def get_paragraphs(db_path):
             paragraphs_to.append(curr_h2-1)
             prev_h2 = curr_h2
 
+        if curr_h3 != prev_h3:
+            paragraphs_from.append(H3_MARK)
+            paragraphs_to.append(curr_h3-1)
+            prev_h3 = curr_h3
+
+        if curr_h4 != prev_h4:
+            paragraphs_from.append(H4_MARK)
+            paragraphs_to.append(curr_h4-1)
+            prev_h4 = curr_h4
+
+        if curr_h5 != prev_h5:
+            paragraphs_from.append(H5_MARK)
+            paragraphs_to.append(curr_h5-1)
+            prev_h5 = curr_h5
+
         if curr_di != prev_di:
             paragraphs_from.append(DIVIDER_MARK)
             paragraphs_to.append(curr_di-1)
@@ -81,10 +100,17 @@ def get_paragraphs(db_path):
     return paragraphs_from, paragraphs_to, meta
 
 
-def create_book(paragraphs_from, paragraphs_to, meta, output_path, template):
+def create_book(paragraphs_from, paragraphs_to, meta, output_path, template, styles=[]):
     """Generate html"""
-    # ensure path is existed 
+    # ensure path is existed
     pathlib.Path(output_path).parent.mkdir(parents=True, exist_ok=True)
+    
+    if template == "custom":
+        css = generate_css(styles)
+        sent_cycle = len(styles)
+    else:
+        css = generate_css([])
+        sent_cycle = 2
 
     with open(output_path, "w", encoding="utf8") as res_html:
         # --------------------HEAD
@@ -92,9 +118,10 @@ def create_book(paragraphs_from, paragraphs_to, meta, output_path, template):
         <html><head>
             <link rel="stylesheet" href="main.css">
             <link rel="preconnect" href="https://fonts.gstatic.com">
-            <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
             <title>Lingtrain Magic Book</title>
-            {CSS_TEMPLATE}
+            {css}
         </head>
         <body>""")
 
@@ -110,73 +137,48 @@ def create_book(paragraphs_from, paragraphs_to, meta, output_path, template):
         # res_html.write("</div></div>")
 
         #--------------------TITLE and AUTHOR
-        res_html.write("<div class='dt-row'><div class='par dt-cell'>")
+        res_html.write("<div class='dt-row header'><div class='par dt-cell'>")
 
-        author_from = get_meta_from(meta, preprocessor.AUTHOR)
-        if author_from:
-            res_html.write("<h1>" + author_from + "</h1>")
         title_from = get_meta_from(meta, preprocessor.TITLE)
         if title_from:
-            res_html.write("<h2>" + title_from + "</h2>")
+            res_html.write("<h1>" + title_from + "</h1>")
+        author_from = get_meta_from(meta, preprocessor.AUTHOR)
+        if author_from:
+            res_html.write("<h2>" + author_from + "</h2>")
 
-        res_html.write("</div>")
+        res_html.write("</div><div class='par dt-cell'>")
 
-        res_html.write("<div class='par dt-cell'>")
-        author_to = get_meta_to(meta, preprocessor.AUTHOR)
-        if author_to:
-            res_html.write("<h1>" + author_to + "</h1>")
         title_to = get_meta_to(meta, preprocessor.TITLE)
         if title_to:
-            res_html.write("<h2>" + title_to + "</h2>")
+            res_html.write("<h1>" + title_to + "</h1>")
+        author_to = get_meta_to(meta, preprocessor.AUTHOR)
+        if author_to:
+            res_html.write("<h2>" + author_to + "</h2>")
 
         res_html.write("</div></div>")
 
-        # --------------------FIRST H1 and H2
-        res_html.write("<div class='dt-row'><div class='par dt-cell'>")
-
-        h1_from = get_meta_from(meta, preprocessor.H1)
-        if h1_from:
-            res_html.write("<h2>" + h1_from + "</h2>")
-        h2_from = get_meta_from(meta, preprocessor.H2)
-        if h2_from:
-            res_html.write("<h3>" + h2_from + "</h3>")
-
+        # --------------------DIVIDER
+        res_html.write("<div class='dt-row header'>")
+        res_html.write(
+            "<div class='dt-cell divider'><img class='divider-img' src='img/divider1.svg'/></div><div class='dt-cell divider'><img class='divider-img' src='img/divider1.svg'/></div>")
         res_html.write("</div>")
 
-        res_html.write("<div class='par dt-cell'>")
-        h1_to = get_meta_to(meta, preprocessor.H1)
-        if h1_to:
-            res_html.write("<h2>" + h1_to + "</h2>")
-        h2_to = get_meta_to(meta, preprocessor.H2)
-        if h2_to:
-            res_html.write("<h3>" + h2_to + "</h3>")
-
-        res_html.write("</div></div>")
+        # --------------------FIRST HEADERS IF EXIST
+        write_header(res_html, meta, preprocessor.H1, occurence=0)
+        write_header(res_html, meta, preprocessor.H2, occurence=0)
+        write_header(res_html, meta, preprocessor.H3, occurence=0)
+        write_header(res_html, meta, preprocessor.H4, occurence=0)
+        write_header(res_html, meta, preprocessor.H5, occurence=0)
 
         # --------------------PARAGRAPHS
         for p_from, p_to in zip(paragraphs_from, paragraphs_to):
-            if p_from == H1_MARK:
-                # left
-                res_html.write("<div class='dt-row'><div class='par dt-cell'>")
-                res_html.write('<h3>' + get_meta_from(meta,
-                                                      preprocessor.H1, p_to) + '</h3>')
-                res_html.write("</div>")
-                # right
-                res_html.write("<div class='par dt-cell'>")
-                res_html.write('<h3>' + get_meta_from(meta,
-                                                      preprocessor.H1, p_to) + '</h3>')
-                res_html.write("</div></div>")
-            elif p_from == H2_MARK:
-                # left
-                res_html.write("<div class='dt-row'><div class='par dt-cell'>")
-                res_html.write('<h4>' + get_meta_from(meta,
-                                                      preprocessor.H2, p_to) + '</h4>')
-                res_html.write("</div>")
-                # right
-                res_html.write("<div class='par dt-cell'>")
-                res_html.write('<h4>' + get_meta_from(meta,
-                                                      preprocessor.H2, p_to) + '</h4>')
-                res_html.write("</div></div>")
+
+            if p_from == H1_MARK: write_header(res_html, meta, preprocessor.H1, occurence=p_to, add_divider=True)
+            elif p_from == H2_MARK: write_header(res_html, meta, preprocessor.H2, occurence=p_to, add_divider=True)
+            elif p_from == H3_MARK: write_header(res_html, meta, preprocessor.H3, occurence=p_to, add_divider=True)
+            elif p_from == H4_MARK: write_header(res_html, meta, preprocessor.H4, occurence=p_to, add_divider=True)
+            elif p_from == H5_MARK: write_header(res_html, meta, preprocessor.H5, occurence=p_to, add_divider=True)
+
             elif p_from == DIVIDER_MARK:
                 res_html.write("<div class='dt-row'>")
                 res_html.write(
@@ -184,13 +186,37 @@ def create_book(paragraphs_from, paragraphs_to, meta, output_path, template):
                 res_html.write("</div>")
             else:
                 res_html.write("<div class='dt-row'><div class='par dt-cell'>")
-                res_html.write(' '.join(p_from))
+                for i, sent in enumerate(p_from):
+                    res_html.write(f"<span class='sent sent-{i%sent_cycle}'>{sent}</span>")
                 res_html.write("</div><div class='par dt-cell'>")
-                res_html.write(' '.join(p_to))
+                for i, sent in enumerate(p_to):
+                    res_html.write(f"<span class='sent sent-{i%sent_cycle}'>{sent}</span>")
                 res_html.write("</div></div>")
 
         res_html.write("</div>")
         res_html.write("</body></html>")
+
+
+def write_header(writer, meta, mark, occurence, add_divider=False):
+    meta_from = get_meta_from(meta, mark, occurence)
+    meta_to = get_meta_to(meta, mark, occurence)
+    if not meta_to: meta_to=meta_from
+
+    if meta_from:
+        if add_divider:
+            writer.write("<div class='dt-row'>")
+            writer.write(
+                "<div class='dt-cell divider'></div><div class='dt-cell divider'></div>")
+            writer.write("</div>")
+        # left
+        writer.write("<div class='dt-row header'><div class='par dt-cell'>")
+        writer.write(f'<{HEADER_HTML_MAPPING[mark]}>' + meta_from + f'</{HEADER_HTML_MAPPING[mark]}>')
+        writer.write("</div>")
+        # right
+        writer.write("<div class='par dt-cell'>")
+        writer.write(f'<{HEADER_HTML_MAPPING[mark]}>' + get_meta_to(meta,
+                                            mark, occurence) + f'</{HEADER_HTML_MAPPING[mark]}>')
+        writer.write("</div></div>")
 
 
 def get_meta_from(meta, mark, occurence=0):
@@ -209,16 +235,62 @@ def get_meta_to(meta, mark, occurence=0):
     return ''
 
 
+def generate_css(styles):
+    special = ""
+    for i,s in enumerate(styles):
+        style = json.loads(s)
+        special += f".sent-{i} {{\n"
+        for rule in style:
+            special += f"{rule}: {style[rule]};"
+        special += "\n}"
+
+    res = CSS_TEMPLATE.replace("%GENERAL%", CSS_GENERAL).replace("%SPECIAL%", special)
+    return res
+
+HEADER_HTML_MAPPING = {
+    preprocessor.H1: "h2",
+    preprocessor.H2: "h3",
+    preprocessor.H3: "h3",
+    preprocessor.H4: "h4",
+    preprocessor.H5: "h4"
+}
+
 CSS_TEMPLATE = """<style type="text/css">
+%GENERAL%
+%SPECIAL%
+</style>"""
+
+CSS_GENERAL = """
 .par {
     font-size: 20px;
     font-family: 'Noto Serif', serif;
-    padding: 30px 60px 0 60px;
+    padding: 15px 60px;
     text-indent: 30px;
+    font-weight: 400;
+}
+
+@media print {
+    .par {
+        font-size: 12px;
+        font-family: 'Noto Serif', serif;
+        padding: 15px 10px;
+        text-indent: 30px;
+        font-weight: 400;
+    }
+}
+
+h1, h2, h3, h4, h5 {
+    font-family: 'Raleway', cursive;
 }
 
 .cont {
     margin: 0 150px;
+}
+
+@media print {
+    .cont {
+        margin: 0 0px;
+    }
 }
 
 .dt {
@@ -229,8 +301,18 @@ CSS_TEMPLATE = """<style type="text/css">
     display: table-row;
 }
 
+.dt-row:nth-child(even):not(.header) {
+    background: #fafafa;
+}
+
+.dt-row:nth-child(even):not(.header) > .dt-cell:not(.divider) {
+    border-top: 1px solid #e0e0e0;
+    border-bottom: 1px solid #e0e0e0;
+}
+
 .dt-cell {
     display: table-cell;
+    width: 50%;
 }
 
 .dt-cell-colspan {
@@ -256,4 +338,8 @@ CSS_TEMPLATE = """<style type="text/css">
 .inline {
     display: inline-block;
 }
-</style>"""
+
+.sent {
+    padding: 0 6px;
+}
+"""
