97a47843320e1efb90d80060ff05ebb6c5458f5a
Sergei Averkiev <averoO@gmail.com>
2021-06-21 10:57:40 +0300

    Update saver module.

--- src/lingtrain_aligner/saver.py
+++ src/lingtrain_aligner/saver.py
@@ -4,22 +4,25 @@ from datetime import datetime
 from lingtrain_aligner import helper
 
 
-def save_tmx(output_path, lang_from, lang_to, text_from, text_to):
+def save_tmx(db_path, output_path, lang_from, lang_to):
     """Save text document in TMX format"""
     tmx_template = TMX_BLOCK.format(timestamp=datetime.utcnow().strftime('%Y%m%dT%H%M%SZ'),
                                     culture_from=get_culture(lang_from), culture_to=get_culture(lang_to))
+    doc_from, doc_to = helper.read_processing(db_path)
     with open(output_path, mode="w", encoding="utf-8") as doc_out:
         doc_out.write(TMX_BEGIN)
-        for f, t in zip(text_from, text_to):
+        for f, t in zip(doc_from, doc_to):
             doc_out.write(tmx_template.format(
                 text_from = f.strip(), text_to = t.strip()))
         doc_out.write(TMX_END)
 
 
-def save_plain_text(output_path, text):
+def save_plain_text(db_path, output_path, direction):
     """Save text document in TXT format"""
+    doc_from, doc_to = helper.read_processing(db_path)
+    to_save = doc_from if direction=="from" else doc_to
     with open(output_path, mode="w", encoding="utf-8") as doc_out:
-        text = "\n".join(text)
+        text = "\n".join(to_save)
         doc_out.write(text)
 
 
