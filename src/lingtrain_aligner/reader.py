2eef7485cdd4d4018ab03d2d324d8a2983a9a3ee
Sergei Averkiev <s.averkiev@kryptonite.ru>
2022-09-12 08:56:50 +0300

    Add meta tag UTF-8.

--- src/lingtrain_aligner/reader.py
+++ src/lingtrain_aligner/reader.py
@@ -570,6 +570,7 @@ def create_book(
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
     <title>Lingtrain Magic Book</title>
+    <meta charset="UTF-8">
     {css}
 </head>
 <body>
@@ -684,6 +685,7 @@ def create_polybook(
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
     <title>Lingtrain Magic Book</title>
+    <meta charset="UTF-8">
     {css}
 </head>
 <body>
