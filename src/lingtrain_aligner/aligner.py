b532f48c63edd5c37b36ee8c481cf8df457bd32d
Sergei Averkiev <averoo@gmail.com>
2023-07-24 15:49:54 +0300

    Fix segments and raise version.

--- src/lingtrain_aligner/aligner.py
+++ src/lingtrain_aligner/aligner.py
@@ -404,7 +404,7 @@ def get_splitted_from_by_par_with_line_id(db_path, par_id_start, par_id_end):
     with sqlite3.connect(db_path) as db:
         res = db.execute(
             f"""select f.id, f.text from splitted_from f
-                                where paragraph > ? and paragraph < ?
+                                where paragraph > ? and paragraph <= ?
                                 order by f.id""",
             (par_id_start, par_id_end),
         ).fetchall()
